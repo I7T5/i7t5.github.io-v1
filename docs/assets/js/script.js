c300bac2e0ef172b7da314c7a5a567e17294e7d8
Ina Tang <inatang75@outlook.com>
2022-10-14 22:31:59 +0800

    modified:   .DS_Store 	modified:   docs/.DS_Store 	modified:   docs/_config.yml 	modified:   docs/_includes/head.html 	new file:   docs/_includes/header.html 	modified:   docs/_includes/sidebar.html 	modified:   docs/_layouts/default.html 	new file:   docs/_layouts/home.html 	renamed:    docs/assets/fonts/.DS_Store -> docs/_pages/.DS_Store 	renamed:    docs/about.md -> docs/_pages/about.md 	modified:   docs/assets/.DS_Store 	modified:   docs/assets/css/.DS_Store 	new file:   docs/assets/css/bootstrap.min.css 	new file:   docs/assets/css/bootstrap.min.css.map 	new file:   docs/assets/css/fonts.css 	new file:   docs/assets/css/fonts/.DS_Store 	new file:   docs/assets/css/fonts/FiraCode/.DS_Store 	new file:   docs/assets/css/fonts/FiraCode/FiraCode-VF.woff2 	new file:   docs/assets/css/fonts/FiraCode/OFL.txt 	new file:   docs/assets/css/fonts/Karla/.DS_Store 	new file:   docs/assets/css/fonts/Karla/Karla-Italic-VariableFont_wght.ttf 	new file:   docs/assets/css/fonts/Karla/Karla-VariableFont_wght.ttf 	new file:   docs/assets/css/fonts/Karla/OFL.txt 	new file:   docs/assets/css/fonts/Lato/.DS_Store 	new file:   docs/assets/css/fonts/Lato/Lato-Bold.ttf 	new file:   docs/assets/css/fonts/Lato/Lato-BoldItalic.ttf 	new file:   docs/assets/css/fonts/Lato/Lato-Italic.ttf 	new file:   docs/assets/css/fonts/Lato/Lato-Light.ttf 	new file:   docs/assets/css/fonts/Lato/Lato-LightItalic.ttf 	new file:   docs/assets/css/fonts/Lato/Lato-Regular.ttf 	new file:   docs/assets/css/fonts/Lato/OFL.txt 	new file:   docs/assets/css/fonts/Mulish/Mulish-Italic-VariableFont_wght.ttf 	new file:   docs/assets/css/fonts/Mulish/Mulish-VariableFont_wght.ttf 	new file:   docs/assets/css/fonts/Mulish/OFL.txt 	new file:   docs/assets/css/fonts/Nunito/.DS_Store 	new file:   docs/assets/css/fonts/Nunito/Nunito-Italic-VariableFont_wght.ttf 	new file:   docs/assets/css/fonts/Nunito/Nunito-VariableFont_wght.ttf 	new file:   docs/assets/css/fonts/Nunito/OFL.txt 	new file:   docs/assets/css/fonts/Philosopher/OFL.txt 	new file:   docs/assets/css/fonts/Philosopher/Philosopher-Bold.ttf 	new file:   docs/assets/css/fonts/Philosopher/Philosopher-BoldItalic.ttf 	new file:   docs/assets/css/fonts/Philosopher/Philosopher-Italic.ttf 	new file:   docs/assets/css/fonts/Philosopher/Philosopher-Regular.ttf 	new file:   docs/assets/css/fonts/WorkSans/.DS_Store 	new file:   docs/assets/css/fonts/WorkSans/OFL.txt 	new file:   docs/assets/css/fonts/WorkSans/WorkSans-Italic-VariableFont_wght.ttf 	new file:   docs/assets/css/fonts/WorkSans/WorkSans-VariableFont_wght.ttf 	new file:   docs/assets/css/home.css 	modified:   docs/assets/css/lanyon.css 	modified:   docs/assets/css/poole.css 	deleted:    docs/assets/fonts/CharisSIL-Regular.woff2 	deleted:    docs/assets/fonts/FiraCode-Regular.woff2 	deleted:    docs/assets/fonts/fonts.css 	new file:   docs/assets/images/expand-verticaliconImage24px.svg 	new file:   docs/assets/images/githubiconImage24px.svg 	deleted:    docs/assets/images/icon-github.svg 	deleted:    docs/assets/images/icon-twitter.svg 	new file:   docs/assets/js/bootstrap.bundle.min.js 	new file:   docs/assets/js/bootstrap.bundle.min.js.map 	modified:   docs/assets/js/script.js 	new file:   docs/blog.html 	modified:   docs/index.html

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,15 +1,100 @@
-(function(document) {
-  var toggle = document.querySelector('.sidebar-toggle');
-  var sidebar = document.querySelector('#sidebar');
-  var checkbox = document.querySelector('#sidebar-checkbox');
+// Sidebar slide
+// (function(document) {
+//   var toggle = document.querySelector('.sidebar-toggle');
+//   var sidebar = document.querySelector('#sidebar');
+//   var checkbox = document.querySelector('#sidebar-checkbox');
 
-  document.addEventListener('click', function(e) {
-    var target = e.target;
+//   document.addEventListener('click', function(e) {
+//     var target = e.target;
 
-    if(!checkbox.checked ||
-       sidebar.contains(target) ||
-       (target === checkbox || target === toggle)) return;
+//     if(!checkbox.checked ||
+//        sidebar.contains(target) ||
+//        (target === checkbox || target === toggle)) return;
 
-    checkbox.checked = false;
-  }, false);
-})(document);
+//     checkbox.checked = false;
+//   }, false);
+// })(document);
+
+
+// Thanks @ths for correcting my function at https://stackoverflow.com/a/74041481/19374566
+function changeFontStyle() {
+  var fontSelector = document.getElementById("font-select");
+  if (fontSelector.value === "") return; // Choose fonts...
+  const fontsJSON = JSON.parse(fontSelector.value);
+  // From least to most specific
+  document.body.style.fontFamily = fontsJSON.bodyFont;
+  document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(h => h.style.fontFamily = fontsJSON.headerFont);
+  // document.getElementsByClassName('masthead-title')[0].style.fontFamily = fontsJSON.bodyFont;
+}
+
+// Typewriting Effect at Homepage
+// Based on https://codepen.io/Coding_Journey/pen/BEMgbX
+
+function typeWriter() {
+
+  const typedTextSpan = document.querySelector(".typed-text");
+  const cursorSpan = document.querySelector(".typing-cursor");
+
+  const textArray = ["Hi there,",
+                      "I'm Ina Tang :D",];  /* The text */
+  const typingDelay = 175;
+  const erasingDelay = 125;
+  const newTextDelay = 1225; // Delay between current and next text
+  let textArrayIndex = 0;
+  let charIndex = 0;
+  var speed = 50;  /* The speed/duration of the effect in milliseconds */
+
+  const nodes = [];
+
+  function type() {
+    cursorSpan.classList.add("typing");
+    if (charIndex < textArray[textArrayIndex].length) {
+      // typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+      cursorSpan.insertAdjacentText('beforeBegin', textArray[textArrayIndex].charAt(charIndex));
+      charIndex++;
+      setTimeout(type, typingDelay);
+    }
+    else if (charIndex == textArray[textArrayIndex].length && textArrayIndex + 1 == textArray.length) {
+      cursorSpan.classList.remove("typing");
+      return;
+    }
+    else {
+      cursorSpan.classList.remove("typing");
+      setTimeout(erase, newTextDelay);
+    }
+  }
+  
+  function erase() {
+    cursorSpan.classList.add("typing");
+    if (charIndex > 0) {
+      // typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+      typedTextSpan.removeChild(cursorSpan.previousSibling);
+      charIndex--;
+      setTimeout(erase, erasingDelay);
+    }
+    else {
+      cursorSpan.classList.remove("typing");
+      textArrayIndex++;
+      setTimeout(type, typingDelay + 1100);
+    }
+  }
+  
+  if(textArrayIndex>=textArray.length) return;
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+  // Initialize fonts
+  document.body.style.fontFamily = "Karla";
+  document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(h => h.style.fontFamily = "Nunito");
+  // document.getElementsByClassName('masthead-title')[0].style.fontFamily = "Karla";
+
+  // Font Select
+  if (document.getElementById("font-select")) document.getElementById("font-select").addEventListener('change', changeFontStyle);
+
+  // Typing Effect
+  if (document.querySelector(".typed-text")) typeWriter();
+});
+
